4a72764c8a2931bad250fb856657344f2f527eab
coderx31 <shenal.fernando10@gmail.com>
2021-06-02 22:05:12 +0530

    cake part done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
+import { Provider } from 'react-redux';
 import './App.css';
+import store from './redux/store';
 import CakeContainer from './components/CakeContainer';
+import HookCakeContainer from './components/HookCakeContainer';
 
 function App() {
   return (
-    <div className="App">
-      <CakeContainer />
-    </div>
+    <Provider store = { store }>
+      <div className="App">
+        <HookCakeContainer />
+        <CakeContainer />
+      </div>
+    </Provider>
+    
   );
 }
 
